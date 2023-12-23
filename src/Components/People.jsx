b6e57e7a2616fc78c679936dafc43fb1501665cb
Natnael Amin <natnaelamin24@gmail.com>
2023-12-23 12:40:04 -0800

    six

--- src/Components/People.jsx
+++ src/Components/People.jsx
@@ -19,7 +19,7 @@ function People({handlePeople, People, tip, customTip, bill}) {
         </div>
         <div className='relative '>
             <input type='number' value={People} onChange={handlePeopleInput} 
-            className='w-full p-2 py-3 text-right  rounded-lg text-teal-950 font-bold' placeholder='0' />
+            className={`w-full p-2 py-3 text-right ${showError && 'border-red-500 border'}  rounded-lg text-teal-950 font-bold`} placeholder='0' />
             <img className='absolute top-4 left-4' src="images/icon-person.svg" alt="person-icon" />
         </div>
     </div>
